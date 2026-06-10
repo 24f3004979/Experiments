0d239837a438e87a046796ddce09577daf501e98
24f3004979 <madhavprapanna@gmail.com>
2026-06-10 09:27:25 +0530

    made first working product componenet with button :)

--- APP/e-com/frontend/src/App.tsx
+++ APP/e-com/frontend/src/App.tsx
@@ -4,6 +4,7 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import Header from './components/navigator.tsx'
+import Product from './components/Products.tsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
       <Header />
+      <Product ProductName="Laptop" price={45} />
     </>
   )
 }
--- APP/e-com/frontend/src/components/Products.tsx
+++ APP/e-com/frontend/src/components/Products.tsx
@@ -1,1 +1,24 @@
-// TODO: Making  simple product component for listing product informaiton
+
+// Product Props required for the componenets fro telling about data flow
+
+interface ProductProps {
+  ProductName: string;
+  price: number;
+}
+
+export default function Product({ ProductName, price }: ProductProps) {
+  return (
+    <div className="Product-Component">
+      <h3>Product Name : {ProductName}</h3>
+      <p> Prize : {price}</p>
+      <button onClick={(): string => AddToCart(ProductName)}>
+        +
+      </button>
+    </div>
+  );
+}
+
+
+function AddToCart(ProductName: string): void {
+  console.log(`Adding to cart : ${ProductName}`);
+}
